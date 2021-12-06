6b8cabd9421056dbeee05a5d11afe808502c9ff1
Jeanderson Candido <jeandersonbc@gmail.com>
2021-12-06 14:58:28 +0100

    added example of concatMap

--- src/test/java/org/example/FlatVsFlatMapTest.java
+++ src/test/java/org/example/FlatVsFlatMapTest.java
@@ -10,7 +10,7 @@ import reactor.test.StepVerifier;
 public class FlatVsFlatMapTest {
 
   @Test
-  @DisplayName("A consumer should be able to ingest the expected number of elements")
+  @DisplayName("A consumer should be able to ingest the expected number of elements asynchronously")
   void checkNumberOfElements() {
     StepVerifier.create(
             Flux.just("abcd", "efg", "hijlm", "nopqrst")
@@ -25,5 +25,28 @@ public class FlatVsFlatMapTest {
         // demonstrate async calls
         .expectNextCount(19)
         .verifyComplete();
+  }
+
+  @Test
+  @DisplayName("A consumer should be able to ingest the expected number of elements in sequence")
+  void checkNumberOfElementsWithConcatMap() {
+    StepVerifier.create(
+            Flux.just("abcd", "efg", "hijlm", "nopqrst")
+                // concatMap is very much similar to flatMap.
+                // Note that the artificial delay is still there.
+                .concatMap(
+                    e ->
+                        Flux.fromArray(e.split(""))
+                            .delayElements(
+                                Duration.ofMillis(RandomGenerator.getDefault().nextLong(1000))))
+                .log())
+        // ...however, concatMap preserves the order of execution - it waits for completion.
+        // for this reason, it is slower than flatMap (given the same delays).
+        .expectNext(
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "q", "r",
+            "s", "t")
+        .verifyComplete();
+
+    // TL;DR: use concatMap when the order matters
   }
 }
